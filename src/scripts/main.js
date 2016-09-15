c5dc2f46e24d724ba182eb2079fc944725194289
0saw <1water4you@gmail.com>
2016-09-15 18:05:48 +0300

    Update.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,70 @@
 $(function() {
-  var frontPage = require('./views/front-page');
+  var enableParallax = null;
+  var helperItems = [];
+
+  $('.literature').on('click', function (e) {
+    $('.footer__legal .footer__cont').show();
+  });
+  $('.readMoreFooter').on('click', function (e) {
+    e.preventDefault();
+    $('.footer__legal .footer__cont').slideToggle();
+  });
+
+  $('.pdSlide').each(function (e) {
+    helperItems.push({
+      elem: this,
+      toggled: false,
+      text: $(this).find('.pdSlide__info'),
+    });
+  }).on('click', function (e) {
+    $this = $(this);
+    var item = $.grep(helperItems, function (elem) {
+      return elem.elem == $this[0];
+    });
+    if (typeof item[0] != 'undefined') {
+      item[0].toggled = true;
+      item[0].text.slideToggle();
+    }
+  });
+
+  function toggleHelpers(show, anim) {
+    if (typeof anim === 'undefined') {
+      anim = true;
+    }
+    $.each(helperItems, function (index, elem) {
+      if (!elem.toggled) {
+        if (anim) {
+          elem.text.slideToggle(show);
+        } else {
+          elem.text.toggle();
+        }
+      }
+    });
+  };
+  if ($(window).height() < 750) {
+    toggleHelpers(false, false);
+  }
+
+  var windowResize = function () {
+    var newParallax = $(window).width() > 960;
+    if (newParallax != enableParallax) {
+      enableParallax = newParallax;
+      if (enableParallax) {
+        toggleHelpers(true);
+      } else {
+        toggleHelpers(false);
+      }
+    }
+  };
+  $(window).on('resize', windowResize);
+
+  var page = $(document.body).hasClass('front');
+  page = page ? 'front' : '';
+  switch (page) {
+    case 'front':
+      var frontPage = require('./views/front-page');
+      break;
+    default:
+      break;
+  }
 });
--- src/scripts/views/front-page.js
+++ src/scripts/views/front-page.js
@@ -61,54 +61,9 @@ var frontPage = function() {
   var $clock = $(".clock");
   var mc = new Hammer(handle);
   var enableParallax = null;
-  var helperItems = [];
   var girlsInfo = {
     x: 0
   };
-
-  $('.literature').on('click', function (e) {
-    $('.footer__legal .footer__cont').show();
-  });
-  $('.readMoreFooter').on('click', function (e) {
-    e.preventDefault();
-    $('.footer__legal .footer__cont').slideToggle();
-  });
-
-  $('.pdSlide').each(function (e) {
-    helperItems.push({
-      elem: this,
-      toggled: false,
-      text: $(this).find('.pdSlide__info'),
-    });
-  }).on('click', function (e) {
-    $this = $(this);
-    var item = $.grep(helperItems, function (elem) {
-      return elem.elem == $this[0];
-    });
-    if (typeof item[0] != 'undefined') {
-      item[0].toggled = true;
-      item[0].text.slideToggle();
-    }
-  });
-
-  function toggleHelpers(show, anim) {
-    if (typeof anim === 'undefined') {
-      anim = true;
-    }
-    $.each(helperItems, function (index, elem) {
-      if (!elem.toggled) {
-        if (anim) {
-          elem.text.slideToggle(show);
-        } else {
-          elem.text.toggle();
-        }
-      }
-    });
-  };
-  console.log($(window).height());
-  if ($(window).height() < 750) {
-    toggleHelpers(false, false);
-  }
 
   var windowResize = function () {
     girlsInfo.width = girlsContainer.offsetWidth;
@@ -118,10 +73,8 @@ var frontPage = function() {
       enableParallax = newParallax;
       if (enableParallax) {
         parallax1.enable();
-        toggleHelpers(true);
       } else {
         parallax1.disable();
-        toggleHelpers(false);
       }
     }
     requestAnimationFrame(pan);
