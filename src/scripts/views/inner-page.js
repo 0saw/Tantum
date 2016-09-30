b1a11ec66578e344cd453c8272fe880bb9285b35
0saw <1water4you@gmail.com>
2016-09-30 10:51:33 +0300

    Update.

--- src/scripts/views/inner-page.js
+++ src/scripts/views/inner-page.js
@@ -1,32 +1,32 @@
 (function () {
-  var $header = $('.header');
-  var $logo = $header.find('.header__logo');
-  var height = $logo.outerHeight();
-  var isFixed = false;
+  // var $header = $('.header');
+  // var $logo = $header.find('.header__logo');
+  // var height = $logo.outerHeight();
+  // var isFixed = false;
 
-  var scrollFun = function () {
-    var scrollTop = $(window).scrollTop();
+  // var scrollFun = function () {
+  //   var scrollTop = $(window).scrollTop();
 
-    if ((isFixed && (scrollTop > height)) || (!isFixed && (scrollTop <= height))) {
-      return;
-    }
+  //   if ((isFixed && (scrollTop > height)) || (!isFixed && (scrollTop <= height))) {
+  //     return;
+  //   }
 
-    if (scrollTop > height) {
-      $header.css('transform', "translate3d(0, -" + $logo.outerHeight() + "px, 0)");
-      $('header').addClass('fixed');
-      isFixed = true;
-    } else {
-      $header.css('transform', "none");
-      $('header').removeClass('fixed');
-      isFixed = false;
-    }
-  }
+  //   if (scrollTop > height) {
+  //     $header.css('transform', "translate3d(0, -" + $logo.outerHeight() + "px, 0)");
+  //     $('header').addClass('fixed');
+  //     isFixed = true;
+  //   } else {
+  //     $header.css('transform', "none");
+  //     $('header').removeClass('fixed');
+  //     isFixed = false;
+  //   }
+  // }
 
-  $(window).on('scroll', scrollFun);
-  $(window).on('resize', function () {
-    height = $logo.outerHeight();
-    scrollFun();
-  });
-  $(document).ready(scrollFun);
+  // $(window).on('scroll', scrollFun);
+  // $(window).on('resize', function () {
+  //   height = $logo.outerHeight();
+  //   scrollFun();
+  // });
+  // $(document).ready(scrollFun);
 
 })();
